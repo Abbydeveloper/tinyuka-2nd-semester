82a13c3051035d2ed7556127266779812ca61f87
Abigail Adeboga <abbyboga@gmail.com>
2025-06-20 08:37:05 +0000

    Add assets, html page, setup webserver

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'webpage')));
 
 app.listen(3000, "0.0.0.0", () => {
-  console.log("Server is up and running");
+  if (error) {
+    console.log("Something is wrong", error);
+  } else {
+    console.log("Server is up and running");
+  }
 })
